f75e26121b73446c1f4db1e34db9c2178ad648b8
Will Mendes <willmendesneto@gmail.com>
2017-06-21 23:33:21 +1000

    ci: adding semantic-release integration

--- package.js
+++ package.js
@@ -3,10 +3,10 @@
 Package.describe({
   name: "willmendesneto:feature-toggle-service",
   summary: "The simplest solution for feature toggle in Javascript. Simple how it should be.",
-  version: "2.0.0",
+  version: "1.2.0",
   git: "https://github.com/willmendesneto/feature-toggle-service.js"
 });
 
 Package.onUse(function(api) {
-  api.addFiles("dist/feature-toggle-service.js", "client");
+  api.addFiles("lib/feature-toggle-service.js", "client");
 });
